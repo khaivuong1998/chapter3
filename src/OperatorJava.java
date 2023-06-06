98a12f951f5af44891eb65042779fc398cd42489
khaivuong1998 <khaivuong311098@gmail.com>
2023-06-06 17:48:44 +0700

    first commit

---
+++ src/OperatorJava.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class OperatorJava {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        Tính diện tích và chu vi hình chữ nhật
+//        int a = 5;
+//        int b = 7;
+//        int S = a * b;
+//        int C = (a + b) * 2;
+//        System.out.println("Diện tích hình chữ nhật: " + S);
+//        System.out.println("Chu vi hình chữ nhật: " + C);
+//        Tính chu giác
+//        int a = 2;
+//        int b = 3;
+//        int c = 5;
+//        int P = a + b + c;
+//        System.out.println("Chu vi hình tam giác: " + P);
+//        Sử dụng toán tử
+//        int a = 3;
+//        int b = 7;
+//        int c = 6;
+//        b += 1;
+//        a -= 2;
+//        c *= 4;
+//        System.out.println(a + " , " + b + " , " + c);
+//        Kiểm tra 2 số có bằng nhau hay ko
+//        System.out.println("Nhập số thứ nhất");
+//        int a = scanner.nextInt();
+//        System.out.println("Nhập số thứ hai");
+//        int b = scanner.nextInt();
+//        if (a == b) {
+//            System.out.println("2 số bằng nhau");
+//        } else {
+//            System.out.println("2 số không bằng nhau");
+//        }
+//        Luỹ thừa của 1 số
+//        System.out.println("Nhập số nguyên bất kỳ");
+//        int a = scanner.nextInt();
+//        int exponentiation = 0;
+//        for (int i = 0; i < 5; i++) {
+//            exponentiation = a * a;
+//        }
+//        System.out.println(exponentiation);
+//        Câu 7
+//        int a = 55;
+//        int b = 70;
+//        if (a < 50 && a < b) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+//        Câu 8
+//        int a = 55;
+//        int b = 70;
+//        if (a < 50 || a < b) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+//        Câu 9
+//        int a = 78;
+//        int b = 45;
+//        int c = 62;
+//        int tong = a + b + c;
+//        double avg = (a + b + c) / 3;
+//        System.out.println("Tổng điểm: " + tong + ", điểm trung bình: " + avg);
+//        Câu 10
+//        int tong = 90;
+//        int nam = 45;
+//        int nu = 45;
+//        Câu 11
+        System.out.println("Mời nhập 5 chữ số");
+        int a = scanner.nextInt();
+    }
+}
